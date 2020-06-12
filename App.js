8ef28a7dfd9d773fa66a52078f5b1981646e6e14
Dakshita <dakshita1997aashi@mail.com>
2020-06-13 03:14:49 +0530

    Navigation working

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -16,11 +14,12 @@ function HomeScreen() {
     </View>
   );
 }
-function DetailsScreen() {
+
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
-            <Button
+      <Button
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
       />
@@ -30,23 +29,18 @@ function DetailsScreen() {
   );
 }
 
-
-
 const Stack = createStackNavigator();
 
-
 function App() {
-
   return (
     <NavigationContainer>
- 
-      <Stack.Navigator initialRouteName="Details">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-   
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
+
